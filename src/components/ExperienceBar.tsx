87196b0b9eea852da577fe7cb05d62a1d9abf8d5
Natalia <natalia-321s@outlook.com>
2021-02-24 02:27:12 -0300

    Components
    - Componente de perfil
    - Componente de barra de experiência
    - Componente de countdown (parcialmente)

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
+export function ExperienceBar(){
+    return(
+        <header className={styles.experienceBar}>
+            <span>8 xp</span>
+            <div>
+                <div style={{width: '50%'}}></div>
+                <span style={{left: '50%'}} className={styles.currentExperience}>
+                    300 xp
+                </span>
+            </div>
+            <span>600 xp</span>
+        </header>
+    )
+}

2c80b3cbad4f2b6803ba3dc5cc37f3f9dad3f3de
Natalia <natalia-321s@outlook.com>
2021-02-26 22:43:41 -0300

    Salvando dados da aplicação via cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
-import '../styles/global.css'
-
-import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext';
+import "../styles/global.css";
+// import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} /> 
-    </ChallengesProvider>
-  )
+  return(
+    <Component {...pageProps} />
+  );
 }
 
-export default MyApp
+export default MyApp;
